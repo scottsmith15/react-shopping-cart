fa0bc3f8bd5d41b80b631468ded0184f3540f344
Scott Smith <scottadamsmith1@gmail.com>
2022-03-17 19:35:56 -0400

    Debugging React Apps

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.value,
   };
 
   // constructor() {
@@ -12,37 +11,33 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    //console.log(product);
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
         <button
           onClick={this.handleIncrement}
           className={"btn btn-secondary btn-sm"}
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
